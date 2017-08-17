294d215b611610fe2e1b1495571f28fa3191a793
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-16 20:52:54 -0400

    set up swiftlint

--- Customers/ViewController.swift
+++ Customers/ViewController.swift
@@ -2,12 +2,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var customerTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        customerTableView.dataSource = self
+        customerTableView.delegate = self
+        
+        title = "Customers"
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5 // change this
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = customerTableView.dequeueReusableCell(withIdentifier: "CustomerCell", for: indexPath)
+        
+        cell.textLabel?.text = "bro"
+    
+        return cell
+          
     }
 
 }
-
