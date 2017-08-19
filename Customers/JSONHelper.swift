1b2beae9e16ef62ea326fe30971093d0fcde430c
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-18 23:59:46 -0400

    working on JSONHelper

--- Customers/JSONHelper.swift
+++ Customers/JSONHelper.swift
@@ -2,4 +2,16 @@ import Foundation
 
 class JSONHelper {
     
+    func extractCustomerData() -> [Any] {
+        return []
+    }
+    
+    func extractItemData() -> [Any] {
+        return []
+    }
+    
+    func extractOrderData() -> [Any] {
+        return []
+    }
+    
 }
